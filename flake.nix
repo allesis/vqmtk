0094176aa85b1010415e7b8a3106f271c459a4ac
Sean Allesina-McGrory <allesis@wwu.edu>
2025-12-09 13:02:00 -0800

    Update nix flake
    Fixes issue of missing deps for vmaf and ms_ssim
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    vqmetric-nix.url = "github:allesis/vqmetric-nix/main";
   };
 
   outputs = {
     self,
     nixpkgs,
+    vqmetric-nix,
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin"];
     forEachSupportedSystem = f:
@@ -23,15 +25,29 @@
         packages = with pkgs; [
           bc
           jq
-libvmaf
+          libvmaf
+          ffmpeg-full
+	  opencv
+          vqmetric-nix.packages.${system}.default
           just
-	  cargo
+          cargo
+          bash-language-server
+          python313Packages.jedi-language-server
         ];
 
-	shellHook = ''
-		cargo install av-metrics-tool
-		export PATH=$HOME/.cargo/bin:$PATH
-	'';
+        nativeBuildInputs = with pkgs; [
+          libvmaf
+          ffmpeg-full
+	  opencv
+          jq
+          bc
+          vqmetric-nix.packages.${system}.default
+        ];
+
+        shellHook = ''
+          export PATH=$HOME/.cargo/bin:$PATH
+          cargo install av-metrics-tool
+        '';
       };
     });
   };
